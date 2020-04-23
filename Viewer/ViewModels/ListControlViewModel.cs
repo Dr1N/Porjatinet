0546b79833d7c357d6d996aa1ef1efff591de269
Dr1N <drn.exp@gmail.com>
2020-04-23 22:23:44 +0300

    adding video list

--- Viewer/ViewModels/ListControlViewModel.cs
+++ Viewer/ViewModels/ListControlViewModel.cs
@@ -9,10 +9,21 @@ namespace Viewer.ViewModels
         private readonly IVideoRepository _repository;
         private readonly IEventAggregator _eventAggregator;
 
+        public BindableCollection<VideoViewModel> List { get; set; } = new BindableCollection<VideoViewModel>();
+
         public ListControlViewModel(IVideoRepository repository, IEventAggregator eventAggregator)
         {
             _repository = repository ?? throw new ArgumentNullException();
             _eventAggregator = eventAggregator ?? throw new ArgumentNullException();
+            InitList();
+        }
+
+        private void InitList()
+        {
+            foreach (var video in _repository.GetAllVideos())
+            {
+                List.Add(new VideoViewModel(video));
+            }
         }
     }
 }
---
+++ Viewer/ViewModels/VideoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Caliburn.Micro;
+using Common.Model;
+using System;
+
+namespace Viewer.ViewModels
+{
+    internal class VideoViewModel : PropertyChangedBase
+    {
+        private readonly Video _video;
+
+        public VideoViewModel(Video video)
+        {
+            _video = video ?? throw new ArgumentNullException(nameof(video));
+        }
+    }
+}
